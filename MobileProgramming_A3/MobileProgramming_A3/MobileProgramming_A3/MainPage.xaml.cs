80722be3f41ee85997a5ca97b267170e0c52e0d3
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2022-07-05 10:12:16 +0300

    Add files via upload

---
+++ MobileProgramming_A3/MobileProgramming_A3/MobileProgramming_A3/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿//@author YD
+using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+
+namespace MobileProgramming_A3
+{
+    public partial class MainPage : ContentPage
+    {  
+        //PhoneBookList = [[]]
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new PhoneBook(), true);
+        }
+
+        async void Button_Clicked_Save(object sender, System.EventArgs e)
+        {
+            if(NameEntry.Text == "" || PhoneEntry.Text == "" || MailEntry.Text == "")
+            {
+                await DisplayAlert("Error!", "You can not leave areas empty!", "OK");
+                return;
+            }
+            else
+            {
+                await DisplayAlert("Success!", "Data Saved Successfully!", "OK");
+                return;
+            }                
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not Supported!", "Your device does not support this functionality!", "OK");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Small
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if(selectedImage == null)
+            {
+                await DisplayAlert("Error!", "Could not upload photo, please try again!", "OK");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+
+
+       
+    }
+}
---
+++ MobileProgramming_A3/MobileProgramming_A3/MobileProgramming_A3/PhoneBook.xaml.cs
@@ -0,0 +1,22 @@
+﻿//@author YD
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileProgramming_A3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhoneBook : ContentPage
+    {
+        public PhoneBook()
+        {
+            InitializeComponent();
+        }      
+
+    }
+}
